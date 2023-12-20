8f233c843412e9b8f6dfbc0ff1704d904be7b611
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-12-20 21:13:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,9 +17,20 @@ const library = [
 ];
 
 const numberOfBooksRead = () => {
-  // write your code here
+  // Initialize a variable to count the number of books read
+  let count = 0;
+
+  // Iterate through the library array
+  for (let i = 0; i < library.length; i++) {
+    // Check if the readingStatus is true (book has been read)
+    if (library[i].readingStatus === true) {
+      count++;
+    }
+  }
+
+  // Return the count of books read
+  return count;
 };
 
-// Do not change the code below
-
-alert(numberOfBooksRead());
+// Use console.log to display the result in the console
+console.log(numberOfBooksRead());
